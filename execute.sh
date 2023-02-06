95038aaf632b3bec76b8cf366bb45876eaf06169
rajeev88in <rajeev88in@yahoo.co.in>
2023-02-06 12:44:34 +0530

    Updated

--- execute.sh
+++ execute.sh
@@ -7,3 +7,13 @@ echo "\n"
 cat alpha.txt
 echo "\n"
 cat execute.sh
+echo "\n"
+cat > ex-1.html <<'EOF'
+  <html>
+  <head>
+    <title>A Placeholder Exercise</title>
+  </head>
+  <body>
+    <h1>My Placeholder album</h1>
+  </body>
+EOF
